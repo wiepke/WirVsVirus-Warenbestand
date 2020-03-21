b31495a6854ea6e001ef95efbc00d55a2edb7dee
Axel Wiepke <wiepke@uni-potsdam.de>
2020-03-21 16:29:27 +0100

    stash

--- WirVsVirus/src/main/java/Service/Calculations.java
+++ WirVsVirus/src/main/java/Service/Calculations.java
@@ -1,4 +1,13 @@
 package Service;
 
+import DataObjects.Market;
+
+import java.util.List;
+
 public class Calculations {
+
+    public List<Market> getCloseByMarkets(float ltdtude, float lngtude){
+
+        return null;
+    }
 }
