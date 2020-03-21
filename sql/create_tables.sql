43f32d28f4f4de4b1335d118a3a3f8404cbc1c35
Baeumla <baeumlandreas@web.de>
2020-03-21 13:52:55 +0100

    sql database v1

---
+++ sql/create_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE market (
+	market_id 			INT, #market ID
+	googleTrack 		VARCHAR(255),
+	name 				VARCHAR(255),
+	address 			VARCHAR(255)
+);
+
+CREATE TABLE products(
+	market_id 		INT, #market ID
+	product_name 	VARCHAR(255),
+	ammount 		INT,
+	recorded_at 	TIMESTAMP
+);
+
+CREATE TABLE market_stats (
+	market_id 		INT, #market ID
+	amountOfPeople 	INT,
+	sizeOfMarket 	INT,
+	averagePresenceTime DECIMAL
+);
